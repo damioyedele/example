21cb501702a8bbae75bd8d6de4b9540e6ff09d3f
Robert Mulholand <rmulholand@8thlight.com>
2017-06-16 15:51:21 -0500

    Add coin changer and calculator exercises

--- src/example.py
+++ src/example.py
@@ -1,6 +1,30 @@
-class Example:
-  def __init__(self):
-    pass
+class Calculator:
+    def __init__(self):
+        pass
 
-  def add_numbers(self, n1, n2):
-    return n1 + n2
+    def add_numbers(self, n1, n2):
+        return n1 + n2
+
+    def subtract_numbers(self, x, y):
+        return x - y
+
+    def multiply_numbers(self, x, y):
+        return x * y
+
+class CoinChanger:
+    def make_change(self, n1, n2):
+        difference = n1 - n2
+        answer = []
+        while difference > 24:
+            answer.append(25)
+            difference = difference - 25
+        while difference > 9:
+            answer.append(10)
+            difference = difference - 10
+        if  difference > 4:
+            answer.append(5)
+            difference = difference - 5
+        while difference > 0:
+            answer.append(1)
+            difference = difference - 1
+        return answer
--- test/test_example.py
+++ test/test_example.py
@@ -1,11 +1,56 @@
 import unittest
 
-from src.example import Example
+from src.example import Calculator, CoinChanger
 
 
-class ExampleTest(unittest.TestCase):
+class CalculatorTest(unittest.TestCase):
 
-  def test_adds_two_numbers(self):
-    adder = Example()
+    def test_adds_two_numbers(self):
+        adder = Calculator()
+        self.assertEqual(3, adder.add_numbers(1, 2))
 
-    self.assertEqual(3, adder.add_numbers(1, 2))
+    def test_adds_two_different_numbers(self):
+        calculator = Calculator()
+        self.assertEqual(4, calculator.add_numbers(2, 2))
+
+    def test_subtracts_two_numbers(self):
+        subtracter = Calculator()
+        self.assertEqual(5, subtracter.subtract_numbers(7, 2))
+
+    def test_multiplies_numbers(self):
+        calculator = Calculator()
+        self.assertEqual(25, calculator.multiply_numbers(5, 5))
+
+    def test_multiplies_other_numbers(self):
+        calculator = Calculator()
+        self.assertEqual(24, calculator.multiply_numbers(6, 4))
+
+class CoinChangerTest(unittest.TestCase):
+
+    def test_provides_a_penny(self):
+        coin_changer = CoinChanger()
+        self.assertEqual([1], coin_changer.make_change(100, 99))
+
+    def test_provides_pennies(self):
+        coin_changer = CoinChanger()
+        self.assertEqual([1, 1, 1], coin_changer.make_change(100, 97))
+
+    def test_provides_more_pennies(self):
+        coin_changer = CoinChanger()
+        self.assertEqual([5, 1, 1, 1], coin_changer.make_change(100, 92))
+
+    def test_provides_dime(self):
+        coin_changer = CoinChanger()
+        self.assertEqual([10], coin_changer.make_change(100, 90))
+
+    def test_provides_two_dimes(self):
+        coin_changer = CoinChanger()
+        self.assertEqual([10, 10], coin_changer.make_change(100, 80))
+
+    def test_provides_a_quarter(self):
+        coin_changer = CoinChanger()
+        self.assertEqual([25], coin_changer.make_change(100, 75))
+
+    def test_does_complex_stuff(self):
+        coin_changer = CoinChanger()
+        self.assertEqual([25, 25, 10, 5, 1, 1], coin_changer.make_change(100, 33))
